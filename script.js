489d9fd0600f55930b46465b26fe0dd233bb304c
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-11-03 10:21:41 -0800

    Added JS for days 3-5

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var cityName = $("#cityName").val();
 // RETRIEVING THE ARRAY OF INFORMATION FROM THE WEATHER API
 $("#weatherBtn").on("click", function weather() {
   cityName = $("#cityName").val().toLowerCase();
-  console.log(weatherURL + cityName + "&appid=" + apiKey);
+  console.log(weatherURL + cityName + "&units=imperial" + "&appid=" + apiKey);
   $.ajax({
     type: "GET",
     url: weatherURL + cityName + "&units=imperial" + "&appid=" + apiKey,
@@ -17,6 +17,10 @@ $("#weatherBtn").on("click", function weather() {
       //APPENDING A DIV TO THE WEBSITE THAT CONTAINS THE WEATHER
       currentWeatherData(result);
       dayOneWeatherData(result);
+      dayTwoWeatherData(result);
+      dayThreeWeatherData(result);
+      dayFourWeatherData(result);
+      dayFiveWeatherData(result);
 
       //   RETRIEVING THE LAST SEARCHED CITIES, IF NULL, ASSIGN A BLANK ARRAY
       var historyList = JSON.parse(localStorage.getItem("cityList")) || [];
@@ -59,19 +63,73 @@ function currentWeatherData(response){
    $("#speed").html("With a wind speed of "+todaysWindSpeed+"mph");
    
 }
-
+// FUNCTION TO LOAD UP THE NEXT DAYS WEATHER
 function dayOneWeatherData(response){
-  console.log(response);
-  var dateOne = response.list[1].dt_txt;
-  var weatherIconOne = response.list[1].weather[0].icon;
-  var tempOne = response.list[1].main.temp;
-  var humidOne = response.list[1].main.humidity;
+  
+  var dateOne = response.list[8].dt_txt;
+  var weatherIconOne = response.list[8].weather[0].icon;
+  var tempOne = response.list[8].main.temp;
+  var humidOne = response.list[8].main.humidity;
   var iconURL = "http://openweathermap.org/img/w/" + weatherIconOne + ".png";
 
   $("#dateOne").html("On "+dateOne);
   $("#iconOne").attr('src',iconURL);
   $("#tempOne").html(tempOne+" Degrees Fahrenheit");
   $("#humidOne").html(humidOne+"%");
+}
+// FUNCTION TO LOAD DAY TWO DATA
+function dayTwoWeatherData(response){
+  var dateTwo = response.list[16].dt_txt;
+  var weatherIconTwo = response.list[16].weather[0].icon;
+  var tempTwo = response.list[16].main.temp;
+  var humidTwo = response.list[16].main.humidity;
+  var iconURL = "http://openweathermap.org/img/w/" + weatherIconTwo + ".png";
+
+  $("#dateTwo").html("On "+dateTwo);
+  $("#iconTwo").attr('src',iconURL);
+  $("#tempTwo").html(tempTwo+" Degrees Fahrenheit");
+  $("#humidTwo").html(humidTwo+"%");
+
+}
+//FUNCTION TO LOAD DAY THREE DATA
+function dayThreeWeatherData(response){
+  var dateThree = response.list[24].dt_txt;
+  var weatherIconThree = response.list[24].weather[0].icon;
+  var tempThree = response.list[24].main.temp;
+  var humidThree = response.list[24].main.humidity;
+  var iconURL = "http://openweathermap.org/img/w/" + weatherIconThree + ".png";
+
+  $("#dateThree").html("On "+dateThree);
+  $("#iconThree").attr('src',iconURL);
+  $("#tempThree").html(tempThree+" Degrees Fahrenheit");
+  $("#humidThree").html(humidThree+"%");
+
+}
+//FUNCTION TO LOAD DAY FOUR DATA
+function dayFourWeatherData(response){
+  var dateFour = response.list[32].dt_txt;
+  var weatherIconFour = response.list[32].weather[0].icon;
+  var tempFour = response.list[32].main.temp;
+  var humidFour = response.list[32].main.humidity;
+  var iconURL = "http://openweathermap.org/img/w/" + weatherIconFour + ".png";
+
+  $("#dateFour").html("On "+dateFour);
+  $("#iconFour").attr('src',iconURL);
+  $("#tempFour").html(tempFour+" Degrees Fahrenheit");
+  $("#humidFour").html(humidFour+"%");
+}
+//FUNCTION TO LOAD DAY FIVE DATA
+function dayFiveWeatherData(response){
+  var dateFive = response.list[40].dt_txt;
+  var weatherIconFive = response.list[40].weather[0].icon;
+  var tempFive = response.list[40].main.temp;
+  var humidFive = response.list[40].main.humidity;
+  var iconURL = "http://openweathermap.org/img/w/" + weatherIconFive + ".png";
+
+  $("#dateFive").html("On "+dateFive);
+  $("#iconFive").attr('src',iconURL);
+  $("#tempFive").html(tempFive+" Degrees Fahrenheit");
+  $("#humidFive").html(humidFive+"%");
 }
 
 function getUVIndex(lat, lon) {
